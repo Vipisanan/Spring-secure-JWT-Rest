7a938e2b9474e383e1bea65bc32fbab2a29140b4
Vipisanan <vipisanan1@gmail.com>
2019-03-28 13:48:36 +0530

    fullyAuthentication

--- src/main/java/com/example/demo/Config/SecurityConfiguration.java
+++ src/main/java/com/example/demo/Config/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.example.demo.Config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,11 +11,20 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("vipi").password("{noop}1234").roles("ADMIN")
+                .and()
+                .withUser("ram").password("{noop}1234").roles("USER");
+    }
+
+    @Override
     public void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeRequests()
 //                .antMatchers("/home")
                 .anyRequest()
-                .permitAll()
+                .fullyAuthenticated()
+//                .permitAll()
                 .and().httpBasic();
         httpSecurity.csrf().disable();
     }
