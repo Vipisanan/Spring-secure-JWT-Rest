17f70b2c67dadbd6132258ba110f9b29be5a1f3a
Vipisanan <vipisanan1@gmail.com>
2019-03-31 07:34:03 +0530

    Basic auth works by role

--- src/main/java/com/example/demo/Config/SecurityConfiguration.java
+++ src/main/java/com/example/demo/Config/SecurityConfiguration.java
@@ -1,10 +1,13 @@
 package com.example.demo.Config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
@@ -13,19 +16,23 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
-                .withUser("vipi").password("{noop}1234").roles("ADMIN")
+                .withUser("vipi").password(passwordEncoder().encode("1234")).roles("ADMIN")
                 .and()
-                .withUser("ram").password("{noop}1234").roles("USER");
+                .withUser("ram").password(passwordEncoder().encode("1234")).roles("USER");
     }
 
     @Override
     public void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeRequests()
-//                .antMatchers("/home")
-                .anyRequest()
-                .fullyAuthenticated()
-//                .permitAll()
-                .and().httpBasic();
-        httpSecurity.csrf().disable();
+        httpSecurity.httpBasic()
+                .and().authorizeRequests()
+                .antMatchers("/user").hasRole("USER")
+                .antMatchers("/admin").hasRole("ADMIN")
+                .and()
+                .csrf().disable();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/com/example/demo/Controller/Hello.java
+++ src/main/java/com/example/demo/Controller/Hello.java
@@ -1,17 +1,50 @@
 package com.example.demo.Controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
-@RequestMapping("hello")
+@RequestMapping()
 public class Hello {
 
-    @GetMapping("/test")
-    public String hello(){
-        System.out.println("++++++++++++++++++++++++++");
-        return "hi vipi!";
+
+    @RequestMapping("/user")
+    public String welcomeUser()
+    {
+        return "User has successfully logged in!!!";
+
+    }
+
+    @RequestMapping("/admin")
+    public String welcomeAdmin()
+    {
+        return "Admin has successfully logged in!!!";
+    }
+
+    @GetMapping("/test-admin")
+    public String hello(@AuthenticationPrincipal UserDetails userDetails) {
+//        String username = userDetails.getUsername();
+//        Collection<? extends GrantedAuthority> authorities = userDetails.getAuthorities();
+//        authorities
+//                .forEach(System.out::println);
+        System.out.println("this is admin URL---------------------------------------------");
+        return "hi admin vipi!";
+    }
+
+    @GetMapping("/test-user")
+    public String hellouser() {
+        System.out.println("this is user URL----------------------------------------------");
+        return "hi user vipi!";
+    }
+
+    @GetMapping("/anyone")
+    public String helloanyone() {
+        System.out.println("this is anyOne URL----------------------------------------------");
+        return "hi anyOne user!";
     }
 }
 
